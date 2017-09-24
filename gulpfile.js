362fe36f49ca061c853d58378914d3a8bc1000a1
shihab993 <syedshihabdu@gmail.com>
2017-09-24 15:43:03 +0600

    image optimization added

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,11 @@ var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
+const imagemin = require('gulp-imagemin');
+var imageminPngquant = require('imagemin-pngquant');
+var imageminZopfli = require('imagemin-zopfli');
+var imageminMozjpeg = require('imagemin-mozjpeg'); //need to run 'brew install libpng'
+var imageminGiflossy = require('imagemin-giflossy');
 
 /* Setup scss path */
 var paths = {
@@ -89,8 +94,51 @@ gulp.task('merge-styles', function () {
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('app/css'))
         .pipe(reload({stream:true}));
-});
+})
 
+/* image optimization*/
+
+gulp.task('images', () =>
+    gulp.src('raw-images/*')
+        .pipe(imagemin(
+		[
+            //png
+            imageminPngquant({
+                speed: 1,
+                quality: 98 //lossy settings
+            }),
+            imageminZopfli({
+                more: true
+            }),
+            //gif
+            // imagemin.gifsicle({
+            //     interlaced: true,
+            //     optimizationLevel: 3
+            // }),
+            //gif very light lossy, use only one of gifsicle or Giflossy
+            imageminGiflossy({
+                optimizationLevel: 3,
+                optimize: 3, //keep-empty: Preserve empty transparent frames
+                lossy: 2
+            }),
+            //svg
+            imagemin.svgo({
+                plugins: [{
+                    removeViewBox: false
+                }]
+            }),
+            //jpg lossless
+            imagemin.jpegtran({
+                progressive: true
+            }),
+            //jpg very light lossy, use vs jpegtran
+            imageminMozjpeg({
+                quality: 90
+            })
+        ]
+		))
+        .pipe(gulp.dest('app/images'))
+);
 
 /* Reload task */
 gulp.task('bs-reload', function () {
@@ -112,11 +160,13 @@ gulp.task('browser-sync', function() {
 });
 
 /* Watch scss, js and html files, doing different things with each. */
-gulp.task('default', ['sass', 'scripts', 'browser-sync', 'merge-styles',], function () {
+gulp.task('default', ['sass', 'scripts', 'browser-sync', 'merge-styles','images'], function () {
     /* Watch scss, run the sass task on change. */
     gulp.watch(['app/scss/*.scss', 'appscss/**/*.scss'], ['sass'])
     /* Watch app.js file, run the scripts task on change. */
     gulp.watch(['app/js/custom.js'], ['minify-custom'])
+	gulp.watch(['raw-images/*'], ['images']);
     /* Watch .html files, run the bs-reload task on change. */
     gulp.watch(['app/*.html'], ['bs-reload']);
+	
 });
