5e90e8972278bb7dfda18ee47a379390d820396c
shihab993 <syedshihabdu@gmail.com>
2017-09-25 14:00:12 +0600

    Update structure of boilerplate

--- README.md
+++ README.md
@@ -4,13 +4,13 @@ Use this organized structure to build a simple site.
 
 
 ## Requeriments
-This project have some requeriments you need to meet in order to compile it. First of all, you need NodeJS in order to run javascript on the console, you can go to the [NodeJS](http://nodejs.rg) site and follow trough the installation process. After you get the `node` command on the console, you need to install Gulp and Bower globally with the following command.
+This project have some requeriments you need to meet in order to compile it. First of all, you need NodeJS in order to run javascript on the console, you can go to the [NodeJS](http://nodejs.rg) site and follow trough the installation process. After you get the `npm` command on the console, you need to install Gulp with the following command.
 
 ```
-node install -g gulp bower
+npm install -g gulp
 ```
 
-Gulp is the one that will run all the compilation, watchers and others tasks. Bower will get the dependencies for the client side like jQuery. Those are the only requeriments to run this project.
+Gulp is the one that will run all the compilation, watchers and others tasks.
 
 ## Install
 In order to start using the project you need to clone it to your pc. You can download the the zip version from [here](#) or clone the project with the git command.
@@ -23,4 +23,20 @@ After you have it on you pc, you need to go in the console to the project folder
 npm install
 ```
 After the proccess finish you will have all you need to start coding.
+
+## How to use
+To start using it, the only thing you need to do is open the project on you code editor of choice and code. To compile and live preview all your changes you have some command that will help you. Here are the list of commands you should know.
+
+Every command have to be executed on the root directory of the project using the gulp command like `gulp clean` or `gulp`
+
+* **default**: Compile and watch for changes (For development)
+* **clean**: Removes all the compiled files on ./app
+* **scripts**: Compile the JavaScript files
+* **sass**: Compile the Sass styles
+* **images**: Copy the newer to the build folder
+* **minify-custom**: minify custom.js files
+* **merge-styles**: compack vendor css files
+* **browsersync**: Start the browsersync server
+
+If you are in development, the `gulp` command is the best choice for you. Go to the project folder in the console and execute `gulp default`, it will compile the project and start a server that will refresh every time you change something in the code. Gulp will be watching for changes and will tell you how to access the project from local and public url. Every browser that point to that url will be auto refreshed. As an extra feature for testing purpose any interaction on one browser will be reflected on any others. Try it on a phone, tablet and pc at the same time.
 
--- gulpfile.js
+++ gulpfile.js
@@ -10,76 +10,142 @@ var concat = require('gulp-concat');
 var plumber = require('gulp-plumber');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
-const sourcemaps = require('gulp-sourcemaps');
-const autoprefixer = require('gulp-autoprefixer');
-const imagemin = require('gulp-imagemin');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+var imagemin = require('gulp-imagemin');
 var imageminPngquant = require('imagemin-pngquant');
 var imageminZopfli = require('imagemin-zopfli');
 var imageminMozjpeg = require('imagemin-mozjpeg'); //need to run 'brew install libpng'
 var imageminGiflossy = require('imagemin-giflossy');
+var del = require('del');
 
-/* Setup scss path */
-var paths = {
-    scss: './sass/*.scss'
+/* Setup mode configaration
+ * prod: false ==== development mode
+ * prod: true  ==== production mode
+ * */
+var config = {
+    prod: false
 };
+
+
+/* clean app folder */
+
+gulp.task('clean', function () {
+    del([
+        'app/*'
+    ]);
+});
 
 /* Scripts task */
 gulp.task('scripts', function() {
-  return gulp.src([
-    /* Add your JS files here, they will be combined in this order */
-    'app/js/vendor/jquery.min.js',
-    'app/js/vendor/*.js',
-    
-    ])
-    .pipe(concat('scripts.js'))
-    .pipe(gulp.dest('app/js'))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(uglify())
-    .pipe(gulp.dest('app/js'));
+    if(!config.prod){
+        return gulp.src([
+            /* Add your JS files here, they will be combined in this order */
+            'src/vendor/js/jquery.min.js',
+            'src/vendor/js/*.js',
+
+        ])
+        .pipe(concat('scripts.js'))
+        .pipe(gulp.dest('app/js'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('app/js'));
+    }else{
+        return gulp.src([
+            /* Add your JS files here, they will be combined in this order */
+            'src/vendor/js/jquery.min.js',
+            'src/vendor/js/*.js',
+
+        ])
+            .pipe(concat('scripts.js'))
+            .pipe(gulp.dest('app/js'))
+            .pipe(rename({suffix: '.min'}))
+            .pipe(uglify())
+            .pipe(gulp.dest('app/js'));
+    }
+
 });
 
 gulp.task('minify-custom', function() {
-  return gulp.src([
-    /* Add your JS files here, they will be combined in this order */
-    'app/js/custom.js'
-    ])
-    .pipe(rename({suffix: '.min'}))
-    .pipe(uglify())
-    .pipe(gulp.dest('app/js'));
+    if(!config.prod){
+        console.log('Custom.js is in Development mode!')
+        return gulp.src([
+            /* Add your JS files here, they will be combined in this order */
+            'src/js/custom.js'
+        ])
+            .pipe(rename({suffix: '.min'}))
+            // .pipe(uglify())
+            .pipe(gulp.dest('app/js'));
+    }else{
+        return gulp.src([
+            /* Add your JS files here, they will be combined in this order */
+            'src/js/custom.js'
+        ])
+            .pipe(rename({suffix: '.min'}))
+            .pipe(uglify())
+            .pipe(gulp.dest('app/js'));
+    }
+
 });
 
 /* Sass task */
 gulp.task('sass', function () {  
-    gulp.src('app/scss/style.scss')
-    .pipe(plumber())
-    .pipe(sass({
-      errLogToConsole: true,
+    if(!config.prod){
+       return gulp.src('src/scss/style.scss')
+            .pipe(plumber())
+            .pipe(sass({
+                errLogToConsole: true,
 
-      //outputStyle: 'compressed',
-      // outputStyle: 'compact',
-      // outputStyle: 'nested',
-      outputStyle: 'expanded',
-      precision: 10
-    }))
+                //outputStyle: 'compressed',
+                // outputStyle: 'compact',
+                // outputStyle: 'nested',
+                outputStyle: 'expanded',
+                precision: 10
+            }))
 
-    .pipe(sourcemaps.init())
-    .pipe(autoprefixer({
-        browsers: ['last 2 versions'],
-        cascade: false
-    }))
-    .pipe(gulp.dest('app/css'))
+            .pipe(sourcemaps.init())
+            .pipe(autoprefixer({
+                browsers: ['last 2 versions'],
+                cascade: false
+            }))
+            .pipe(gulp.dest('app/css'))
 
-    .pipe(rename({suffix: '.min'}))
-    .pipe(minifycss())
-    .pipe(gulp.dest('app/css'))
-    /* Reload the browser CSS after every change */
-    .pipe(reload({stream:true}));
+            .pipe(rename({suffix: '.min'}))
+            // .pipe(minifycss())
+            .pipe(gulp.dest('app/css'))
+            /* Reload the browser CSS after every change */
+            .pipe(browserSync.reload({stream:true}));
+    }else{
+       return gulp.src('src/scss/style.scss')
+            .pipe(plumber())
+            .pipe(sass({
+                errLogToConsole: true,
+
+                //outputStyle: 'compressed',
+                // outputStyle: 'compact',
+                // outputStyle: 'nested',
+                outputStyle: 'expanded',
+                precision: 10
+            }))
+
+            .pipe(sourcemaps.init())
+            .pipe(autoprefixer({
+                browsers: ['last 2 versions'],
+                cascade: false
+            }))
+            .pipe(gulp.dest('app/css'))
+
+            .pipe(rename({suffix: '.min'}))
+            .pipe(minifycss())
+            .pipe(gulp.dest('app/css'))
+            /* Reload the browser CSS after every change */
+            .pipe(browserSync.reload({stream:true}));
+    }
 });
 
 gulp.task('merge-styles', function () {
 
     return gulp.src([
-        'app/css/vendor/*.css',
+        'src/vendor/css/*.css',
         
         ])
         // .pipe(sourcemaps.init())
@@ -93,52 +159,61 @@ gulp.task('merge-styles', function () {
         // .pipe(minifycss())
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('app/css'))
-        .pipe(reload({stream:true}));
+        .pipe(browserSync.reload({stream:true}));
 })
 
 /* image optimization*/
 
-gulp.task('images', () =>
-    gulp.src('raw-images/*')
-        .pipe(imagemin(
-		[
-            //png
-            imageminPngquant({
-                speed: 1,
-                quality: 98 //lossy settings
-            }),
-            imageminZopfli({
-                more: true
-            }),
-            //gif
-            // imagemin.gifsicle({
-            //     interlaced: true,
-            //     optimizationLevel: 3
-            // }),
-            //gif very light lossy, use only one of gifsicle or Giflossy
-            imageminGiflossy({
-                optimizationLevel: 3,
-                optimize: 3, //keep-empty: Preserve empty transparent frames
-                lossy: 2
-            }),
-            //svg
-            imagemin.svgo({
-                plugins: [{
-                    removeViewBox: false
-                }]
-            }),
-            //jpg lossless
-            imagemin.jpegtran({
-                progressive: true
-            }),
-            //jpg very light lossy, use vs jpegtran
-            imageminMozjpeg({
-                quality: 90
-            })
-        ]
-		))
-        .pipe(gulp.dest('app/images'))
-);
+gulp.task('images', function () {
+     return gulp.src('src/img/*')
+            .pipe(imagemin(
+                [
+                    //png
+                    imageminPngquant({
+                        speed: 1,
+                        quality: 98 //lossy settings
+                    }),
+                    imageminZopfli({
+                        more: true
+                    }),
+                    //gif
+                    // imagemin.gifsicle({
+                    //     interlaced: true,
+                    //     optimizationLevel: 3
+                    // }),
+                    //gif very light lossy, use only one of gifsicle or Giflossy
+                    imageminGiflossy({
+                        optimizationLevel: 3,
+                        optimize: 3, //keep-empty: Preserve empty transparent frames
+                        lossy: 2
+                    }),
+                    //svg
+                    imagemin.svgo({
+                        plugins: [{
+                            removeViewBox: false
+                        }]
+                    }),
+                    //jpg lossless
+                    imagemin.jpegtran({
+                        progressive: true
+                    }),
+                    //jpg very light lossy, use vs jpegtran
+                    imageminMozjpeg({
+                        quality: 90
+                    })
+                ]
+            ))
+            .pipe(gulp.dest('app/images'))
+});
+
+/* html from src to app */
+
+gulp.task('rander-view', function () {
+    return gulp.src('src/index.html')
+        .pipe(gulp.dest('app'))
+        .pipe(browserSync.reload({stream:true}));
+});
+
 
 /* Reload task */
 gulp.task('bs-reload', function () {
@@ -147,26 +222,27 @@ gulp.task('bs-reload', function () {
 
 /* Prepare Browser-sync for localhost */
 gulp.task('browser-sync', function() {
-    browserSync.init(['css/*.css', 'js/*.js'], {
+    browserSync.init(['app/css/*.css', 'app/js/*.js'], {
         
-        proxy: 'localhost/boilerplate/app'
+        // proxy: 'localhost/boilerplate/app'
         /* For a static server you would use this: */
-        /*
+
         server: {
-            baseDir: './'
+            baseDir: './app',
+            index: 'index.html'
         }
-        */
+
     });
 });
 
 /* Watch scss, js and html files, doing different things with each. */
-gulp.task('default', ['sass', 'scripts', 'browser-sync', 'merge-styles','images'], function () {
+gulp.task('default', ['sass', 'scripts','minify-custom','rander-view', 'browser-sync', 'merge-styles','images'], function () {
     /* Watch scss, run the sass task on change. */
-    gulp.watch(['app/scss/*.scss', 'appscss/**/*.scss'], ['sass'])
+    gulp.watch(['src/scss/style.scss','src/scss/**/*.scss'], ['sass'])
     /* Watch app.js file, run the scripts task on change. */
-    gulp.watch(['app/js/custom.js'], ['minify-custom'])
-	gulp.watch(['raw-images/*'], ['images']);
+    gulp.watch(['src/js/custom.js'], ['minify-custom'])
+	gulp.watch(['src/img/*'], ['images']);
     /* Watch .html files, run the bs-reload task on change. */
-    gulp.watch(['app/*.html'], ['bs-reload']);
+    gulp.watch(['src/index.html'], ['rander-view']);
 	
 });
